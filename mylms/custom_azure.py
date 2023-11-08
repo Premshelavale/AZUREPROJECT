7863045e16836f26e2453901b2e0f42ab8cbf178
Premshelavale <146810922+Premshelavale@users.noreply.github.com>
2023-11-08 20:38:09 +0530

    custom_azure.py

--- mylms/custom_azure.py
+++ mylms/custom_azure.py
@@ -2,15 +2,15 @@ from storages.backends.azure_storage import AzureStorage
 from django.conf import settings
 
 class AzureMediaStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME # Must be replaced by your <storage_account_name>
-    account_key = settings.AZURE_STORAGE_ACCOUNT_KEY # Must be replaced by your <storage_account_key>
+    account_name = settings.lmsstorage12 # Must be replaced by your <storage_account_name>
+    account_key = settings.TrA/zCzBAKt7wplnoWxU9g+sPYBXHnGEiNGFx+AR6RiHxwdx0dGEpgjLvnN0ithUaTZgaJQcoYXl+ASt3qY2hg== # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
     # file_overwrite = False
 
 class AzureStaticStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME # Must be replaced by your storage_account_name
-    account_key = settings.AZURE_STORAGE_ACCOUNT_KEY # Must be replaced by your <storage_account_key>
+    account_name = settings.lmsstorage12 # Must be replaced by your storage_account_name
+    account_key = settings.TrA/zCzBAKt7wplnoWxU9g+sPYBXHnGEiNGFx+AR6RiHxwdx0dGEpgjLvnN0ithUaTZgaJQcoYXl+ASt3qY2hg== # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
     # file_overwrite = False
